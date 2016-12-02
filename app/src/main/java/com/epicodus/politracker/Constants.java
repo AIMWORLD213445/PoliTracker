eed84c1a4af927a3951f0b59be12fb75c18043a4
Brian Dutz <bgdutz@gmail.com>
2016-12-02 08:55:45 -0800

    create constants to handle API key and queries

---
+++ app/src/main/java/com/epicodus/politracker/Constants.java
@@ -0,0 +1,11 @@
+package com.epicodus.politracker;
+
+/**
+ * Created by Guest on 12/2/16.
+ */
+public class Constants {
+    public static final String GIANT_BOMB_KEY = BuildConfig.GIANT_BOMB_KEY;
+    public static final String GIANT_BOMB_URL ="http://www.giantbomb.com/api/search/";
+    public static final String GAME_SEARCH="query";
+    public static final String API_KEY_QUERY_PARAMETER="api_key";
+}
